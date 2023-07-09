99ecca79f36c165fb41b512914b8b025363654c5
shibanee94 <shibanee.sai94@gmail.com>
2023-07-09 17:12:10 +0530

    Added project

---
+++ src/components/FormField.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const FormField = ({
+  LabelName,
+  type,
+  name,
+  placeHolder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div className="flex items-center gap-2 mb-2"> 
+    <label 
+    htmlFor={name}
+    className="block text-sm font-medium text-gray-900"
+    >
+      {LabelName}
+    </label>
+    {isSurpriseMe && (
+      <button 
+      type="button"
+      onClick={handleSurpriseMe}
+      className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+      >
+       Surprise Me
+      </button>
+    )}
+    </div>
+  )
+};
+
+export default FormField;
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
+
+const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setform] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setgeneratingImg] = useState(false);
+  const [loading, setloading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Create imaginative and visually stunning images through DALL-E AI and
+          share them with the community
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField 
+          LabelName="Your Name"
+          type = "text"
+          name="name"
+          placeHolder="Name"
+          value={form.name}
+          handleChange={handleChange}
+          />
+
+          <FormField 
+          LabelName="Prompt"
+          type="text"
+          name="prompt"
+          placeHolder="'an astronaut lounging in a tropical resort in space, vaporwave',"
+          value={form.prompt}
+          handleChange={handleChange}
+          isSurpriseMe
+          handleSurpriseMe={handleSurpriseMe}
+          />
+
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default CreatePost;
